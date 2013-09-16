ae853d0cdccf3e5a35aa24d18bfda4f533172bdf
Ross Donaldson <gastove@gmail.com>
2013-09-16 08:50:20 -0700

    Blow away everything left in master.

--- .hyde.el
+++
@@ -1,8 +0,0 @@
-(setq hyde/git/remote "origin"
-      hyde-home "~/code/personal_projects/blag"
-      hyde-deploy-dir "_deploy"
-      hyde-posts-dir "source/_posts"
-      hyde-drafts-dir "source/_drafts"
-      hyde/deploy-command "rake deploy"
-      hyde/jekyll-command "rake generate"
-)
